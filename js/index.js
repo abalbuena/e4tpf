38c3df45c6aa98820b1c983b987390a7a2163fc8
Ángel Balbuena <angelbalbuena@MacBook-Pro-de-Angel.local>
2023-12-11 19:23:57 -0300

    ajuste de imagenes de album

--- js/index.js
+++ js/index.js
@@ -2,6 +2,7 @@
 var generarImagen = function() {
     console.log("se va a generar la imagen")
     html2canvas(document.querySelector("#list_container")).then(canvas => {
+        document.getElementById("imagen_generada_container").innerHTML = "";
         document.getElementById("imagen_generada_container").appendChild(canvas)
     });
 }
